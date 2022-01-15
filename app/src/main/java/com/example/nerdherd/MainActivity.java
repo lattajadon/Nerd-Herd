7342d3ca6c4fb9c66fcfaa3af42fa8d1d3a679b9
lattajadon <lattajadon@gmail.com>
2022-01-15 15:27:01 -0700

    Testing new Git option

---
+++ app/src/main/java/com/example/nerdherd/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nerdherd;
+
+import
+        androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
